e6d881f0fd88f81d7c3c31fa1cd566288479c891
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-09-02 03:14:01 +0600

    all task done primarily

--- js/book-archive.js
+++ js/book-archive.js
@@ -7,10 +7,38 @@ const loadBooks = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data.docs));
-    // console.log(searchText);
-}
+    toggleSpinner('block');
+};
+//function to display books
 const displayBooks = books => {
+    const showResults = document.getElementById('show-results');
+    showResults.textContent = '';
+    // total results 
+    if (books.length === 0) {
+        document.getElementById('results-found').innerHTML = `<h3 class="text-center text-danger">No Results found</h3>`;
+    }
+    else {
+        document.getElementById('results-found').innerHTML = `<h3 class="text-center text-danger">${books.length} Results found</h3>`;
+    }
+    //forEach to find every book
     books.forEach(book => {
-        console.log(book);
-    })
-}
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="this is cover for ${book.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${book.title}</h5>
+                        <p class="card-text">Author: ${book.author_name}</p>
+                        <p class="card-text">The first edition was published in ${book?.first_publish_year ?? 'Unknown'} year by ${book?.publisher ?? 'An Unknown Publisher'}</p>
+                    </div>
+                </div>
+        `
+        showResults.appendChild(div);
+    });
+    toggleSpinner('none');
+};
+//loading spinner 
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+};
