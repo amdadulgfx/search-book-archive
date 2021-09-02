82b9e3282093e51f68011075b838e33f6545176d
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-09-02 14:10:41 +0600

    updated coding all site

--- js/book-archive.js
+++ js/book-archive.js
@@ -3,10 +3,12 @@ const loadBooks = () => {
     const searchField = document.getElementById('search-input');
     const searchText = searchField.value;
     searchField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data.docs));
+    document.getElementById('show-results').textContent = '';
+    document.getElementById('results-found').textContent = '';
     toggleSpinner('block');
 };
 //function to display books
@@ -20,17 +22,19 @@ const displayBooks = books => {
     else {
         document.getElementById('results-found').innerHTML = `<h3 class="text-center text-danger">${books.length} Results found</h3>`;
     }
-    //forEach to find every book
-    books.forEach(book => {
+    //forEach to find every book 
+    books.slice(0, 20).forEach(book => {
+        //adding the book to the search result 
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
-                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="this is cover for ${book.title}">
+        <div class="card h-100">
+                    <img height="400" src="https://covers.openlibrary.org/b/id/${book?.cover_i ?? 'no image'}-M.jpg" class="card-img-top" alt="this is cover for ${book.title}">
                     <div class="card-body">
                         <h5 class="card-title">${book.title}</h5>
-                        <p class="card-text">Author: ${book.author_name}</p>
-                        <p class="card-text">The first edition was published in ${book?.first_publish_year ?? 'Unknown'} year by ${book?.publisher ?? 'An Unknown Publisher'}</p>
+                        <p class="card-text">Author: ${book?.author_name ?? ''}</p>
+                        <p class="card-text">First Published: ${book?.first_publish_year ?? 'Unknown'}  </p>
+                        <p class="card-text">Publisher: ${book?.publisher ?? ''}</p>
                     </div>
                 </div>
         `
